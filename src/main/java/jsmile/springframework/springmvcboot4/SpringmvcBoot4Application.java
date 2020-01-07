42f8ea3e29b2f0dd7b0e37f505034903f15b9df8
Byeong Gui Jeong <jsmile@naver.com>
2020-01-07 12:51:49 +0900

    Spring Framework MVC Base & index

---
+++ src/main/java/jsmile/springframework/springmvcboot4/SpringmvcBoot4Application.java
@@ -0,0 +1,15 @@
+package jsmile.springframework.springmvcboot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcBoot4Application
+{
+
+   public static void main( String[] args )
+   {
+      SpringApplication.run( SpringmvcBoot4Application.class, args );
+   }
+
+}
---
+++ src/test/java/jsmile/springframework/springmvcboot4/SpringmvcBoot4ApplicationTests.java
@@ -0,0 +1,15 @@
+package jsmile.springframework.springmvcboot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcBoot4ApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
