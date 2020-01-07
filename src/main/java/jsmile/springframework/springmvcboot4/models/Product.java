9a9d373bd45726959533e8b191a30fd237eeedd7
Byeong Gui Jeong <jsmile@naver.com>
2020-01-07 17:43:55 +0900

    Listing Products

---
+++ src/main/java/jsmile/springframework/springmvcboot4/models/Product.java
@@ -0,0 +1,57 @@
+package jsmile.springframework.springmvcboot4.models;
+
+import java.math.BigDecimal;
+
+public class Product
+{
+   private Integer id;
+   private String description;
+   private BigDecimal price;
+   private String imageUrl;
+
+   public Integer getId()
+   {
+      return id;
+   }
+
+   public void setId( Integer id )
+   {
+      this.id = id;
+   }
+
+   public String getDescription()
+   {
+      return description;
+   }
+
+   public void setDescription( String description )
+   {
+      this.description = description;
+   }
+
+   public BigDecimal getPrice()
+   {
+      return price;
+   }
+
+   public void setPrice( BigDecimal price )
+   {
+      this.price = price;
+   }
+
+   public String getImageUrl()
+   {
+      return imageUrl;
+   }
+
+   public void setImageUrl( String imageUrl )
+   {
+      this.imageUrl = imageUrl;
+   }
+
+   @Override
+   public String toString()
+   {
+      return "Product{" + "id=" + id + ", description='" + description + '\'' + ", price=" + price + ", imageUrl='" + imageUrl + '\'' + '}';
+   }
+}
